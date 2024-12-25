65a168fc8614d617052f9dfa6860385fedf86c94
himanshujiit <himanshujiit2018@gmail.com>
2024-12-25 01:57:29 -0800

    changes till 25-12-24

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,7 +12,25 @@
     // Initialize WebSocket connections
     // Start matchmaking worker
 
-	http.HandleFunc("/matchmaking/add",matchmaking.AddPlayerToQueue);
+	//health check route
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("this is healthcheck")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	//matchmaking add user
+	http.HandleFunc("/matchmaking/add",func(w http.ResponseWriter,r* http.Request){
+		log.Println("add user for matchmaking")
+		matchmaking.AddPlayerToQueue(w,r)
+	})
+    // root level endpoints
+	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+		log.Println("websocket received");
+		session.HandleWebSocket(w,r) // what is the meaning of this
+	})
+	
+
+	//http.HandleFunc("/matchmaking/add",matchmaking.AddPlayerToQueue);
 	http.HandleFunc("/ws",session.HandleWebSocket)
 
 	log.Println("Server is running at 8080..")
